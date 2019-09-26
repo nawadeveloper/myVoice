3dcf199bec57366b871e415a50737815c4e2c9b9
nawadeveloper <nawarajusa@gmail.com>
2019-09-25 22:09:29 -0400

    recyclerview setup for friend fragment

---
+++ app/src/main/java/np/com/nawarajbista/myvoice/FriendSuggestion.kt
@@ -0,0 +1,20 @@
+package np.com.nawarajbista.myvoice
+
+
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+
+class FriendSuggestion: Item<GroupieViewHolder>() {
+
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+
+        return R.layout.friend_suggestion
+    }
+
+}
--- app/src/main/java/np/com/nawarajbista/myvoice/ui/friends/FriendsFragment.kt
+++ app/src/main/java/np/com/nawarajbista/myvoice/ui/friends/FriendsFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.lifecycle.Observer
-
+import androidx.recyclerview.widget.AdapterListUpdateCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import kotlinx.android.synthetic.main.fragment_friends.*
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import np.com.nawarajbista.myvoice.FriendSuggestion
 import np.com.nawarajbista.myvoice.R
 
 class FriendsFragment : Fragment() {
@@ -24,11 +28,19 @@ class FriendsFragment : Fragment() {
         friendsViewModel =
             ViewModelProviders.of(this).get(FriendsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_friends, container, false)
-        val textView: TextView = root.findViewById(R.id.friends)
+        //val textView: TextView = root.findViewById(R.id.friends)
         friendsViewModel.text.observe(this, Observer {
-            textView.text = it
+            //textView.text = it
         })
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adaptor = GroupAdapter<GroupieViewHolder>()
+        adaptor.add(FriendSuggestion())
+        recyclerview_friend_suggestion.adapter = adaptor
     }
 
 }
